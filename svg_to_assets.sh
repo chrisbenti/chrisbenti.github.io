be23f3f1d4122307752d43b908fe5e105059d1fb
chrisbenti <chrisbenti@users.noreply.github.com>
2023-01-02 23:32:30 +0000

    deploy: 414908ec24b01f75853c3de1cda18ecbeac011cb

---
+++ svg_to_assets.sh
@@ -0,0 +1,2 @@
+convert -density 256x256 -background transparent favicon.svg -define icon:auto-resize -colors 256 favicon.ico
+convert -density 256x256 -background transparent favicon.svg favicon.png
